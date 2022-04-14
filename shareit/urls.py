7732bb792825367c32831a42a9677f036f6f188d
WisamTa <wisamtaqaly@gmail.com>
2022-04-14 20:13:46 +0000

    starting new workspace and install django allover again and make some changing

--- shareit/urls.py
+++ shareit/urls.py
@@ -14,7 +14,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+urlpatterns = [
+@@ -23,3 +25,6 @@
+    path('accounts/', include('allauth.urls')),
+    path('socialnetwork/', include('socialnetwork.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
