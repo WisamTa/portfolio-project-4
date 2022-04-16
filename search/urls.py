f08a369f4674de13aa9643b1b2318441fcbf8d92
WisamTa <wisamtaqaly@gmail.com>
2022-04-16 08:35:47 +0000

    Fix search bar and style it, styling uplaoded post and profile page, and footer

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import Search
+from socialnetwork.views import UserProfile
 
 urlpatterns = [
     path('search/', Search.as_view(), name='search_user'),
+    path('search/<int:pk>', UserProfile.as_view(), name='search_user'),
 ]
--- socialnetwork/models.py
+++ socialnetwork/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from dj.choices import Choices, Choice
 from dj.choices.fields import ChoiceField
@@ -47,22 +46,14 @@ class Users(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
     picture = CloudinaryField('image', default='placeholder')
     birthday = models.DateField(blank=True, null=True)
-    gender = models.IntegerField(choices=Gender(), default=Gender.other.name)
+    gender = models.IntegerField(choices=Gender(), default=Gender.other.id)
     location = models.CharField(max_length=100, blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
     followers = models.ManyToManyField(User, blank=True, related_name='followers')
 
     def number_of_followers(self):
         return self.followers.count()
-    
-    def user_gender(self):
-        gender = Gender.from_id(self.gender)
-        if gender == Gender.male:
-            return 'Male'
-        elif gender == Gender.female:
-            return 'Female'
-        else:
-            return 'Other'
+
 
 
 """
@@ -73,7 +64,6 @@ for not getting errors when loggin in after created a new user.
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Users.objects.create(user=instance)
-
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
